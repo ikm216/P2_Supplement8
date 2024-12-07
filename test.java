dbc953cf551bc93b314319f460b6896421d27bda
ikm216 <ikmclassof22@gmail.com>
2024-12-07 18:40:05 -0500

    test: added test to converted local date to iso.

--- test.java
+++ test.java
@@ -1,4 +1,7 @@
 import org.junit.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.*;
 
 public class test{
@@ -6,5 +9,11 @@ public class test{
     public void test_should_return_current_date_time(){
         ZonedDateTime currentDT = time.currentDateTime();
         Assert.assertNotNull(currentDT);    
+    }
+    @Test
+    public void test_should_return_converted_local_date(){
+        LocalDate date = LocalDate.of(2024, 12, 3);
+        String convertedDate = time.convertedLocalDate(date);
+        Assert.assertEquals("2024-12-03T00:00:00Z", convertedDate);
     }
 }
