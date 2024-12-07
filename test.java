f7107e52191b7c8f8e259c95493367531fbffd7c
ikm216 <ikmclassof22@gmail.com>
2024-12-07 18:06:24 -0500

    test: added test to get current date time

--- test.java
+++ test.java
@@ -1,5 +1,10 @@
 import org.junit.*;
+import java.time.*;
 
 public class test{
-    
+    @Test
+    public void test_should_return_current_date_time(){
+        ZonedDateTime currentDT = time.currentDateTime();
+        Assert.assertNotNull(currentDT);    
+    }
 }
