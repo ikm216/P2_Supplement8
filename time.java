350790c31f1470db28a17ad980b6acc429d6ec52
ikm216 <ikmclassof22@gmail.com>
2024-12-07 18:33:10 -0500

    doc: added documentation feature to get current date time.

--- time.java
+++ time.java
@@ -1,6 +1,11 @@
 import java.time.*;
 
 public class time {
+    /**
+     * Returns the current date and time.
+     * 
+     * @return ZonedDateTime - The current date and time in system's default timezone.
+     */
     public static ZonedDateTime currentDateTime(){
         return ZonedDateTime.now();
     }
